98b795a9e7fe48c7da7575c56e2b060215ac07c0
Matias Ramos <matiasramos@gmail.com>
2022-07-30 10:20:27 -0300

    fotos

--- src/utilities/pieces.js
+++ src/utilities/pieces.js
@@ -7,7 +7,7 @@ const pieces = [
         measures: '12x18',
         year: 2012,
         price: '3223 USD',
-        image: ''
+        image: 'https://res.cloudinary.com/dna8yz36g/image/upload/v1659136980/obras-yuyi/DSC_0021_unukcg.jpg'
     },
 
     {
@@ -17,7 +17,7 @@ const pieces = [
         measures: '25x32',
         year: 2006,
         price: '3223 USD',
-        image: ''
+        image: 'https://res.cloudinary.com/dna8yz36g/image/upload/v1659136980/obras-yuyi/DSC_0015_dubkj5.jpg'
     },
 
     {
@@ -27,7 +27,7 @@ const pieces = [
         measures: '20x70',
         year: 2022,
         price: '3223 USD',
-        image: ''
+        image: 'https://res.cloudinary.com/dna8yz36g/image/upload/v1659136979/obras-yuyi/DSC_0019_sql6mf.jpg'
     },
 
     {
@@ -37,7 +37,27 @@ const pieces = [
         measures: '10x40',
         year: 2019,
         price: '3223 USD',
-        image: ''
+        image: 'https://res.cloudinary.com/dna8yz36g/image/upload/v1659136978/obras-yuyi/DSC_0024_mcfat6.jpg'
+    },
+
+    {
+        id: 5,
+        title: 'hmbnmb',
+        technique: 'acuarela',
+        measures: '10x40',
+        year: 2019,
+        price: '3223 USD',
+        image: 'https://res.cloudinary.com/dna8yz36g/image/upload/v1659136978/obras-yuyi/DSC_0017_ioobqu.jpg'
+    },
+
+    {
+        id: 6,
+        title: 'fghyuhjn',
+        technique: 'acuarela',
+        measures: '10x40',
+        year: 2019,
+        price: '3223 USD',
+        image: 'https://res.cloudinary.com/dna8yz36g/image/upload/v1659136977/obras-yuyi/DSC_0023_q2cmuz.jpg'
     },
 
 
