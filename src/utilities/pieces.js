a2152e0121799441119eada78002bd76289533f5
Matias Ramos <matiasramos@gmail.com>
2022-08-08 18:14:18 -0300

    nuevas fotos

--- src/utilities/pieces.js
+++ src/utilities/pieces.js
@@ -5,7 +5,7 @@ const pieces = [
         title: 'Invierno',
         series: 'Estaciones',
         technique: 'Tinta',
-        measures: '0.55x0.45',
+        measures: '55cm x 45cm',
         year: 2022,
         image: 'https://res.cloudinary.com/dna8yz36g/image/upload/v1659136980/obras-yuyi/DSC_0021_unukcg.jpg'
     },
@@ -15,7 +15,7 @@ const pieces = [
         title: 'Equinoccios',
         series: 'Chacana Dorada',
         technique: 'Mixta',
-        measures: '0.50x0.40',
+        measures: '50cm x 40cm',
         year: 2022,
         image: 'https://res.cloudinary.com/dna8yz36g/image/upload/v1659136980/obras-yuyi/DSC_0015_dubkj5.jpg'
     },
@@ -25,9 +25,9 @@ const pieces = [
         title: 'Cruz del Sur',
         series: 'Chacana Dorada',
         technique: 'Mixta',
-        measures: '0.40x0.50',
+        measures: '40cm x 50cm',
         year: 2022,
-        image: 'https://res.cloudinary.com/dna8yz36g/image/upload/v1659136979/obras-yuyi/DSC_0019_sql6mf.jpg'
+        image: 'https://res.cloudinary.com/dna8yz36g/image/upload/v1659993118/obras-yuyi/DSC_0019_ec79v0.jpg'
     },
 
     {
@@ -35,9 +35,9 @@ const pieces = [
         title: 'La Guardia',
         series: null,
         technique: 'Acrílico',
-        measures: '0.30x0.40',
+        measures: '30cm x 40cm',
         year: 2016,
-        image: 'https://res.cloudinary.com/dna8yz36g/image/upload/v1659136978/obras-yuyi/DSC_0024_mcfat6.jpg'
+        image: 'https://res.cloudinary.com/dna8yz36g/image/upload/v1659993130/obras-yuyi/DSC_0024_qsewwy.jpg'
     },
 
     {
@@ -45,7 +45,7 @@ const pieces = [
         title: 'Chaska (Estrella)',
         series: 'Chacana Dorada',
         technique: 'Mixta',
-        measures: '0.50x0.40',
+        measures: '50cm x 40cm',
         year: 2022,
         image: 'https://res.cloudinary.com/dna8yz36g/image/upload/v1659136978/obras-yuyi/DSC_0017_ioobqu.jpg'
     },
@@ -55,9 +55,9 @@ const pieces = [
         title: 'Sinfonía en Flor',
         series: null,
         technique: 'Óleo',
-        measures: '0.40x0.55',
+        measures: '40cm x 55cm',
         year: 2018,
-        image: 'https://res.cloudinary.com/dna8yz36g/image/upload/v1659136977/obras-yuyi/DSC_0023_q2cmuz.jpg'
+        image: 'https://res.cloudinary.com/dna8yz36g/image/upload/v1659993118/obras-yuyi/DSC_0023_eq8xqp.jpg'
     },
 
 
