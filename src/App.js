aa0d29f26708145232f5abd7f574291cec449a01
Matias Ramos <matiasramos@gmail.com>
2023-03-23 19:01:49 -0300

    pathname

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import { useState } from 'react';
 import './styles/app/app.css';
 import Router from './components/Router'
 import PresentationModal from './components/PresentationModal';
+import { useLocation } from 'react-router-dom';
 
 function App() {
 
   const [show, setShow] = useState(true)
+
+  const { pathname } = useLocation()
+
+  console.log(pathname)
 
   return (
     <div className="App">
